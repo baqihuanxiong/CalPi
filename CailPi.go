d72a3df5612466cf7f633695ab4b55e5b27d1c7f
slw <lw390937635@163.com>
2019-08-04 16:29:18 +0800

    v0.2.0

--- CailPi.go
+++ CailPi.go
@@ -8,40 +8,51 @@ import (
 	"github.com/garyburd/redigo/redis"
 	"github.com/golang/glog"
 	uuid "github.com/satori/go.uuid"
+	"github.com/valyala/fastrand"
 	"google.golang.org/grpc"
 	"io/ioutil"
 	"math"
-	"math/rand"
 	"net"
+	"os"
 	"runtime"
 	"time"
 )
 
 type Config struct {
-	HostName   string `json:"HostName"`
-	GrpcAddr   string `json:"grpcAddr"`
-	RedisAddr  string `json:"redisAddr"`
-	GOMAXPROCS int    `json:"GOMAXPROCS"`
+	HostName       string `json:"hostName"`
+	GrpcAddr       string `json:"grpcAddr"`
+	RedisAddr      string `json:"redisAddr"`
+	GOMAXPROCS     int    `json:"GOMAXPROCS"`
+	TASKCHANLENGTH int    `json:"TASKCHANLENGTH"`
 }
 
 type Result struct {
-	Id      int64
-	Darts   int64
-	Hits    int64
-	Consume int64
+	Id        int64
+	Darts     int64
+	Hits      int64
+	Consume   int64
+	Timestamp int64
 }
 
 var (
 	hostName    string
 	config      Config
+	cpuNum      int
+	taskRunChan chan *rcmsg.CalRequest
 	taskInChan  chan *rcmsg.CalRequest
 	taskOutChan chan *Result
+	runningProc chan int
 )
 
 type RPCServer struct{}
 
 func (s *RPCServer) MonteCarlo(ctx context.Context, in *rcmsg.CalRequest) (*rcmsg.CalReply, error) {
+	if len(taskInChan) == config.TASKCHANLENGTH {
+		glog.V(4).Infoln("gRPC server reject MonteCarlo request, running process:", len(runningProc))
+		return &rcmsg.CalReply{Id: -1, Timestamp: time.Now().UnixNano()}, nil
+	}
 	taskInChan <- in
+	glog.V(4).Infoln("gRPC server get MonteCarlo request:", in)
 	return &rcmsg.CalReply{Id: in.Id, Timestamp: time.Now().UnixNano()}, nil
 }
 
@@ -65,14 +76,25 @@ func main() {
 	} else {
 		hostName = config.HostName
 	}
+	cpuNum = runtime.NumCPU()
 	if config.GOMAXPROCS > 0 {
-		runtime.GOMAXPROCS(config.GOMAXPROCS)
+		cpuNum = config.GOMAXPROCS
 	}
+
+	taskRunChan = make(chan *rcmsg.CalRequest, 1)
+	taskInChan = make(chan *rcmsg.CalRequest, config.TASKCHANLENGTH)
+	taskOutChan = make(chan *Result, config.TASKCHANLENGTH)
+	runningProc = make(chan int, cpuNum)
 
 	c, err := redis.Dial("tcp", config.RedisAddr)
 	if err != nil {
 		panic(err)
 	}
+	err = RedisSetWorkNode(c)
+	if err != nil {
+		panic(err)
+	}
+	glog.V(3).Infoln("Work node", hostName, "established, detect", runtime.NumCPU(), "cpus")
 
 	lis, err := net.Listen("tcp", config.GrpcAddr)
 	if err != nil {
@@ -88,16 +110,25 @@ func main() {
 	}()
 	glog.V(3).Infoln("gRPC server listening on", config.GrpcAddr)
 
+	go func() {
+		for {
+			runningProc <- 1
+			t := <-taskInChan
+			taskRunChan <- t
+		}
+	}()
+
 	for {
 		select {
-		case req := <-taskInChan:
+		case req := <-taskRunChan:
 			go CalPi(req.Id, req.Darts)
-			glog.V(4).Infoln("Started task %d", req.Id)
+			glog.V(4).Infoln("Started task", req.Id)
 		case result := <-taskOutChan:
 			err := RedisSetPiResult(c, result)
 			if err != nil {
 				glog.Errorln("Write redis error:", err)
 			}
+			glog.V(4).Infoln("Task", result.Id, "finished")
 		}
 	}
 }
@@ -107,20 +138,33 @@ func CalPi(id, darts int64) {
 	result := &Result{Id: id, Darts: darts, Hits: 0}
 	var i int64
 	for i = 0; i < darts; i++ {
-		x, y := rand.Float64(), rand.Float64()
+		x, y := float64(fastrand.Uint32n(1e9))/1e9, float64(fastrand.Uint32n(1e9))/1e9
 		if math.Sqrt(x*x+y*y) < 1 {
 			result.Hits++
 		}
 	}
 	result.Consume = time.Since(start).Nanoseconds()
+	result.Timestamp = time.Now().UnixNano()
 	taskOutChan <- result
+	<-runningProc
+}
+
+func RedisSetWorkNode(redisConn redis.Conn) error {
+	osHostName, _ := os.Hostname()
+	_, err := redisConn.Do("HMSET", "PiTemp:WorkNodes", hostName, osHostName)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func RedisSetPiResult(redisConn redis.Conn, result *Result) error {
 	baseKeyName := "PiTemp:" + hostName
 	_ = redisConn.Send("MULTI")
-	_ = redisConn.Send("MSET", baseKeyName+":ID", result.Id,
-		baseKeyName+":Darts", result.Darts, baseKeyName+":Consume", result.Consume)
+	_ = redisConn.Send("MSET", baseKeyName+":Timestamp", result.Timestamp,
+		baseKeyName+":RunningProc", len(runningProc))
+	_ = redisConn.Send("INCRBY", baseKeyName+":Consume", result.Consume)
+	_ = redisConn.Send("INCRBY", baseKeyName+":Darts", result.Darts)
 	_ = redisConn.Send("INCRBY", baseKeyName+":Hits", result.Hits)
 	_ = redisConn.Send("EXEC")
 	_, err := redisConn.Do("")
--- rcmsg/dispatcher.py
+++ rcmsg/dispatcher.py
@@ -1,5 +1,80 @@
 import grpc
+import redis
+import time
+import argparse
+import logging
+import numpy as np
+import pandas as pd
 import message_pb2
 import message_pb2_grpc
 
 
+logging.basicConfig(format='%(asctime)s - %(levelname)s: %(message)s', level=logging.DEBUG)
+
+parser = argparse.ArgumentParser(description='CalPi task dispatcher.')
+parser.add_argument("-g", "--grpc-addr", type=str, default="127.0.0.1:50051", help="gRPC server address")
+parser.add_argument("-r", "--redis-addr", type=str, default="127.0.0.1:6379", help="redis server address")
+parser.add_argument("-d", "--redis-db", type=int, default=0, help="redis db")
+parser.add_argument("-D", "--darts", type=int, default=int(1e8), help="Darts per task")
+parser.add_argument("-i", "--interval", type=int, default=1, help="interval seconds")
+parser.add_argument("-n", "--num", type=int, default=0, help="number of tasks, infinite if zero")
+parser.add_argument("-t", "--timeout", type=int, default=0, help="stop send task when timeout(second) reach, infinite if zero")
+args = parser.parse_args()
+
+
+class Dispatcher:
+    def __init__(self, grpc_addr, redis_addr):
+        self._chan = grpc.insecure_channel(grpc_addr)
+        self._stub = message_pb2_grpc.CalculatorStub(self._chan)
+        redis_host = redis_addr.split(":")[0]
+        redis_port = int(redis_addr.split(":")[1])
+        self._redis = redis.Redis(host=redis_host, port=redis_port, db=0)
+    
+    def send(self, darts):
+        tid = self._redis.get("PiTemp:tid")
+        if tid is None:
+            tid = 0
+        req = message_pb2.CalRequest(id=int(tid), darts=darts, timestamp=time.time_ns())
+        res = self._stub.MonteCarlo(req)
+        if res.id == -1:
+            logging.warning("Task pending because server is busy, server time {}".format(res.timestamp))
+            return
+        self._redis.incrby("PiTemp:tid", 1)
+        logging.debug("Send task {} success, server time {}".format(res.id, res.timestamp))
+
+    def query_pi(self):
+        cols = ["Consume", "Darts", "Hits", "Timestamp"]
+        result = pd.DataFrame(columns=cols, dtype=np.int64)
+        nodes = [node.decode() for node in self._redis.hgetall("PiTemp:WorkNodes")]
+        tmp = self._redis.mget(["PiTemp:"+node+":"+col for col in cols for node in nodes])
+        for i, col in enumerate(cols):
+            result[col] = [np.nan] * len(nodes)
+            for j, v in enumerate(tmp[i*len(nodes):(i+1)*len(nodes)]):
+                if v is not None:
+                    result[col][j] = int(v.decode())
+        result.index = nodes
+        return result
+
+
+def run():
+    dispatcher = Dispatcher(args.grpc_addr, args.redis_addr)
+    logging.info("Dispatcher start working...")
+    count = 0
+    start_time = time.time()
+    while args.num == 0 or (args.num > 0 and count < args.num):
+        if args.timeout > 0 and time.time() - start_time > args.timeout:
+            break
+        dispatcher.send(args.darts)
+        time.sleep(args.interval)
+        result = dispatcher.query_pi()
+        pi = 4*(sum(result["Hits"])/sum(result["Darts"])) if sum(result["Darts"]) > 0 else np.inf
+        logging.info("Working node: {}, work time: {} s, efficiency: {} darts/s, timestamp: {}, current pi is {}".format(
+            list(result.index), list(result["Consume"]), list(result["Darts"]/result["Consume"]), list(result["Timestamp"]), pi))
+        count += 1
+    logging.info("Done.")
+    
+
+if __name__ == '__main__':
+    run()
+    
+    
